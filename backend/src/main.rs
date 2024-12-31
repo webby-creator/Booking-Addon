53a981632d026bf97f6289388afc24cd4fe9c8d0
Its <2779546+Its-its@users.noreply.github.com>
2024-12-30 22:51:08 -0800

    Return all hours in a staffs' schedule

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,7 +3,10 @@ extern crate tracing;
 
 use std::net::SocketAddr;
 
-use addon_common::{request::query_cms_rows, JsonResponse, WrappingResponse};
+use addon_common::{
+    request::{get_cms_row_by_id, query_cms_rows},
+    JsonResponse, WrappingResponse,
+};
 use axum::{
     extract::{Path, Query},
     routing::get,
@@ -40,7 +43,8 @@ async fn main() -> Result<()> {
     axum::serve(
         listener,
         Router::new()
-            .route("/:uuid/available", get(get_available))
+            .route("/:uuid/availableDays", get(get_available_days))
+            .route("/:uuid/availableHours", get(get_available_hours))
             // .route("/:uuid/book", post(post_booking))
             .layer(TraceLayer::new_for_http()),
     )
@@ -49,11 +53,13 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+// TODO: Account for start time being larger than end time.
+// Eg: Start 11pm -> End 5am
+
 #[derive(serde::Deserialize)]
-struct GetAvailableQuery {
+struct GetAvailableDaysQuery {
     year: usize,
     month: u8,
-    day: Option<usize>,
 }
 
 #[derive(serde::Deserialize)]
@@ -63,9 +69,9 @@ struct RecurrenceRule {
     interval: usize,
 }
 
-async fn get_available(
+async fn get_available_days(
     Path(uuid): Path<UuidType>,
-    Query(query): Query<GetAvailableQuery>,
+    Query(query): Query<GetAvailableDaysQuery>,
 ) -> Result<JsonResponse<serde_json::Value>> {
     // TODO: Remember Daylight Savings Time
 
@@ -99,19 +105,170 @@ async fn get_available(
     // 2024-12-06 | 10:00:00 - 18:00:00 America/Los_Angeles | 1/wk
     // Fridays: Nov 1, 8, 15, 22, 29 | Dec 6, 13, 20, 27
 
-    // TODO: If day is specified, return available hours in the day
-
     let available_days = gather_available_days(now, staff_schedule_resp.items)?;
 
     // TODO: Simplify
     Ok(Json(WrappingResponse::okay(serde_json::json!({
         "timeZone": "America/Los_Angeles",
-        "availableDays": available_days
+        "available": available_days,
+    }))))
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct GetAvailableHoursQuery {
+    day: u8,
+    month: u8,
+    year: usize,
+    schedule_ids: String,
+}
+
+async fn get_available_hours(
+    Path(uuid): Path<UuidType>,
+    Query(GetAvailableHoursQuery {
+        day,
+        month,
+        year,
+        schedule_ids,
+    }): Query<GetAvailableHoursQuery>,
+) -> Result<JsonResponse<serde_json::Value>> {
+    // TODO: Remember Daylight Savings Time
+
+    let list_date =
+        time::Date::from_calendar_date(year as i32, time::Month::try_from(month)?, day)?.midnight();
+
+    let mut staff_schedule = get_cms_row_by_id(
+        uuid,
+        CollectionName {
+            id: String::from("staffSchedule"),
+            ns: Some(String::from("@booking")),
+        },
+        // TODO: Multiple ids can be in here.
+        &schedule_ids,
+    )
+    .await?;
+
+    let schedule = get_cms_row_by_id(
+        uuid,
+        CollectionName {
+            id: String::from("schedule"),
+            ns: Some(String::from("@booking")),
+        },
+        &staff_schedule
+            .fields
+            .get(&SchematicFieldKey::Other(String::from("schedule")))
+            .context("Schedule ID")?
+            .any_as_text()?,
+    )
+    .await?;
+
+    let service = get_cms_row_by_id(
+        uuid,
+        CollectionName {
+            id: String::from("services"),
+            ns: Some(String::from("@booking")),
+        },
+        &schedule
+            .fields
+            .get(&SchematicFieldKey::Other(String::from("service")))
+            .context("Service ID")?
+            .any_as_text()?,
+    )
+    .await?;
+
+    // TODO: Get bookings for the start-end time for the staff schedule(s)
+
+    let duration = Duration::minutes(
+        schedule
+            .fields
+            .get(&SchematicFieldKey::Other(String::from("duration")))
+            .context("Service Duration")?
+            .try_as_number()?
+            .convert_f64() as i64,
+    );
+
+    let break_ = Duration::minutes(
+        schedule
+            .fields
+            .get(&SchematicFieldKey::Other(String::from("break")))
+            .context("Service Break")?
+            .try_as_number()?
+            .convert_f64() as i64,
+    );
+
+    // schedule.fields.get(&SchematicFieldKey::Other(String::from("serviceSchedule"))) (not used yet)
+    // schedule.fields.get(&SchematicFieldKey::Other(String::from("repeats")))
+
+    // service.fields.get(&SchematicFieldKey::Other(String::from("maxParticipants")))
+    // service.fields.get(&SchematicFieldKey::Other(String::from("priceAmount")))
+    // service.fields.get(&SchematicFieldKey::Other(String::from("paymentType")))
+    // service.fields.get(&SchematicFieldKey::Other(String::from("name")))
+    // service.fields.get(&SchematicFieldKey::Other(String::from("type")))
+
+    // println!("{staff_schedule:#?}");
+    // println!("{service:#?}");
+
+    let mut available_hours = Vec::new();
+
+    {
+        // let date_format = format_description!("[year]-[month]-[day]");
+        let time_format = format_description!("[hour]:[minute]:[second]");
+
+        // let start_date = staff_schedule
+        //     .fields
+        //     .remove(&SchematicFieldKey::Other(String::from("startDay")))
+        //     .context("Missing startDay field")?;
+
+        // TODO: We're currently assuming that start time < end time. We need to add a date to both of them.
+        let start_time = staff_schedule
+            .fields
+            .remove(&SchematicFieldKey::Other(String::from("start")))
+            .context("Missing start field")?
+            .try_as_text()?
+            .replace(".0", "");
+
+        let end_time = staff_schedule
+            .fields
+            .remove(&SchematicFieldKey::Other(String::from("end")))
+            .context("Missing end field")?
+            .try_as_text()?
+            .replace(".0", "");
+
+        // let start_date = time::Date::parse(&start_date.try_as_text()?, &date_format).unwrap();
+        let start_time = time::Time::parse(&start_time, &time_format).unwrap();
+        let end_time = time::Time::parse(&end_time, &time_format).unwrap();
+
+        // TODO: Add "break" field eg. 45 min duration, 15 min break = 60 minutes total for one staff service.
+
+        let local_offset = offset!(-7);
+
+        // We don't convert to UTC since start_time & end_time is in local offset time.
+        // TODO: Remove UTC offset, for some reason JavaScript Date is applying the PST offset to the formatted string - need to fix the backend
+        let mut current_time_pos = list_date
+            .replace_time(start_time)
+            .assume_offset(UtcOffset::UTC);
+
+        // Loop until we hit the end of time
+        while current_time_pos.time() + duration + break_ <= end_time {
+            available_hours.push(serde_json::json!({
+                "start": current_time_pos.format(&time::format_description::well_known::Iso8601::DEFAULT).unwrap(),
+                "end": (current_time_pos + duration).format(&time::format_description::well_known::Iso8601::DEFAULT).unwrap(),
+            }));
+
+            current_time_pos += duration + break_;
+        }
+    }
+
+    // let available_hours = gather_available_hours(list_date, staff_schedule)?;
+
+    Ok(Json(WrappingResponse::okay(serde_json::json!({
+        "timeZone": "America/Los_Angeles",
+        "available": available_hours,
     }))))
 }
 
 fn gather_available_days(
-    now: PrimitiveDateTime,
+    lookup_time: PrimitiveDateTime,
     staff_schedule_items: Vec<CmsRowResponse>,
 ) -> Result<Vec<serde_json::Value>> {
     let mut available_days = Vec::new();
@@ -174,7 +331,7 @@ fn gather_available_days(
                 pos = pos.saturating_add(freq);
 
                 // If we're in the current month, we can add it to the list.
-                if now.month() == pos.month() {
+                if lookup_time.month() == pos.month() {
                     found.push(pos);
 
                     // There should only ever be a MAX of 5 weeks in a month.
@@ -184,7 +341,7 @@ fn gather_available_days(
                     }
                 }
                 // If we passed the current month, we can stop.
-                else if pos.month() as u8 > now.month() as u8 {
+                else if pos.month() as u8 > lookup_time.month() as u8 {
                     break;
                 }
                 // If we're still in the past, we can skip.
@@ -247,3 +404,44 @@ fn frequency_str_to_duration(frequency: &str) -> Result<Duration> {
         v => return Err(eyre::eyre!("Invalid frequency: {v}"))?,
     })
 }
+
+// Start DateTime ID
+// TODO: Chars [32 start time][1 version][3 duration][1 recurrence][3 original utc offset]
+struct BookingId {
+    /// Stores the DateTime in which the booking starts in UTC format.
+    start_time: Uuid,
+    /// Version of the BookingID
+    version: u8,
+    /// The duration of the booking in minutes.
+    duration: u32,
+    /// The recurrence of the booking.
+    recurrence: u8,
+    /// The original offset for the start time.
+    /// Since its' currently stored as UTC.
+    utc_offset: u32,
+}
+
+// let start_id = Uuid::new_v7(uuid::Timestamp::from_unix(
+//     uuid::NoContext,
+//     utc.unix_timestamp() as u64,
+//     0,
+// ));
+
+// impl serde::Serialize for BookingId {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: serde::Serializer,
+//     {
+//         let mut this = self.start_time.simple().to_string();
+
+//         // format!("{:X}", 42)
+//         // i64::from_str_radix("1f", 16);
+
+//         // self.version
+//         // self.duration
+//         // self.recurrence
+//         // self.utc_offset
+
+//         Ok(this)
+//     }
+// }
